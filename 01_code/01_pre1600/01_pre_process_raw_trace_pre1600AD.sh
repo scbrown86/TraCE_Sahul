7e194b124fd2b0d8c822b2d10232b3d41d41fd54
Stuart Brown <s.brown@adelaide.edu.au>
2025-06-20 19:56:16 +0930

    decadal calcs

--- 01_code/01_pre1600/01_pre_process_raw_trace_pre1600AD.sh
+++ 01_code/01_pre1600/01_pre_process_raw_trace_pre1600AD.sh
@@ -56,7 +56,7 @@ for file in ./*/*.nc; do
     rm -f "$regridded"
 done
 
-cd "$output_root"
+# cd "$output_root"
 
 # Loop directly over the subdirectories of $output_dir and concatenate
 for folder_path in "$output_root"/*/; do
@@ -72,15 +72,72 @@ for folder_path in "$output_root"/*/; do
         echo "Processing $folder_name with $file_count files…"
         output_file="${folder_path}/trace.01-36.22000BP-1990CE.cam2.h0.${folder_name}.0000101-2204012.Sahul.concat.nc"
         # Concatenate in that exact order
-        ncrcat --4 -t 100 "${nc_files[@]}" -o "$output_file"
-        # add in the time dimension
-        last_date="1989-12-16 00:00:00" # final timestep
-        #ndays_per_mon=30 # 365/12
-        ncap2 -O -s 'nt=$time.size; time[$time]=-(nt-1)+time[$time];' "$output_file" "$output_file"
-        ncatted -O -a units,time,o,c,"months since 1989-12-16" -a calendar,time,o,c,"365_day" -a axis,time,o,c,"T" -a long_name,time,o,c,"time" "$output_file"
+        cdo -f nc4 -P 36 -O cat "${nc_files[@]}" "$output_file"
+        # correct the time dimension
+        ncap2 -O -s 'time=array(-264479,1,$time)' "$output_file" "$output_file"
+        ncatted -O -a units,time,o,c,"months before 1989-12-16" -a calendar,time,o,c,"365_day" -a axis,time,o,c,"T" -a long_name,time,o,c,"time" "$output_file"
         # add calendar month and year helper variable
-        ncap2 -O -s 'nt=$time.size; month[$time]=1 + ((nt-1-time[$time]) - 12*((nt-1-time[$time])/12)); year[$time]=1989 - ((nt-1 - time[$time])/12); month@long_name="calendar month (1–12)"; year@long_name="calendar year";' "$output_file" "$output_file"
+        ncap2 -O -s 'base_yr=1989; month[$time]=12 - ((-time[$time]) % 12); month@long_name="calendar month (1-12)"; year[$time]=base_yr + long(time[$time]/12); year@long_name="calendar year";' "$output_file" "$output_file"
     else
-        echo "Skipping $folder_name — found $file_count files, expected 36"
+        echo "Skipping $folder_name — found $file_count files, expected 36"
     fi
+done
+
+# folder for storing decadal outputs
+mkdir -p /media/dafcluster4/storage/TraCE_Decadal/
+
+for concat_file in "$output_root"/*/*Sahul.concat.nc; do
+    echo $concat_file
+    filepath=$(dirname "$concat_file")
+    var=$(echo basename "$concat_file" | cut -f 6 -d ".")
+    mkdir -p "/media/dafcluster4/storage/TraCE_Decadal/$var"
+    outroot="/media/dafcluster4/storage/TraCE_Decadal/$var"
+    output_file="${outroot}/trace.01-35.22000BP-1500CE.cam2.h0.${var}.0000101-258600.Sahul.decavg.concat.nc"
+    # if final final exists, skip
+    if [[ -f "$output_file" ]]; then
+        echo "Skipping ${var} – already processed: $output_file"
+        continue
+    fi
+    # else, loop through decades here
+    for ((step = 1; step <= 258600; step += 120)); do
+        #echo "$step"
+        end=$((step + 119))
+        #echo "$end"
+        outfile="${outroot}/decadal_mon_avg_${step}_${end}.nc"
+        if [[ -f "$outfile" ]]; then
+            echo "Skipping – already processed: $outfile"
+            continue
+        fi
+        # generate temporary file of 120 steps
+        # Then calculate the multiyear-monthly mean of each of these files
+        tmpfile="$filepath"/temp_decadal_${step}.nc
+        # time is irrelevant here, just for assigning months
+        cdo -w -s ymonmean -settaxis,2000-01-16,,1month -setcalendar,365_day -seltimestep,$step/$end,1 "$concat_file" "$tmpfile"
+        # now correct the time scale. start and end years stored as helper variables.
+        mid=$(((step + end) / 2))
+        ncap2 -O -s 'time=array(0.083f,0.083f,$time)' "$tmpfile" "$tmpfile"
+        ncap2 -O -s "base_dec=$mid" "$tmpfile" "$tmpfile"
+        ncap2 -O -s 'base_dec=base_dec.convert(NC_FLOAT);' "$tmpfile" "$tmpfile"
+        ncap2 -O -s 'time=time+base_dec;' "$tmpfile" "$tmpfile"
+        ncap2 -O -s "start_cal_year=$step; end_cal_year=$end; mid_cal_year=$mid" "$tmpfile" "$tmpfile"
+        ncatted -O -a units,time,o,c,"years from 22ka BP" -a calendar,time,o,c,"365_day" -a axis,time,o,c,"T" -a long_name,time,o,c,"time" "$tmpfile"
+        # outfile
+        cp $tmpfile $outfile
+        ncks -O --glb input_steps=${step}-${end} $outfile $outfile
+        ncks -O --glb infile=${concat_file} $outfile $outfile
+        rm -f $tmpfile
+    done
+    # now concatenate the files
+    # mapfile -t nc_files < <(ls -v1 "$outroot"/*.nc 2>/dev/null)
+    # # Save the order to text for checking
+    # printf '%s\n' "${nc_files[@]}" >"${outroot}/input_order.txt"
+    # file_count=${#nc_files[@]}
+    # if ((file_count == 2155)); then # if all timesteps are present, then concatenate
+    # echo "Processing $outroot with $file_count files…"
+    # output_file="${outroot}/trace.01-35.22000BP-1500CE.cam2.h0.${var}.0000101-258600.Sahul.decavg.concat.nc"
+    # # Concatenate in that exact order
+    # cdo -f nc4 -P 36 -O cat "${nc_files[@]}" "$output_file"
+    # else
+    # echo "Skipping $folder_name — found $file_count files, expected 36"
+    # fi
 done
