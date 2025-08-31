777c4dc26a0da583c6d6aa586a3da8290d62a803
Stuart Brown <s.brown@adelaide.edu.au>
2025-08-31 13:02:22 +0930

    final bias correction concatenate

--- 01_code/01_Decadal_pre1500/05_bias_correction_dtr_method.sh
+++ 01_code/01_Decadal_pre1500/05_bias_correction_dtr_method.sh
@@ -36,9 +36,10 @@ conda deactivate || true
 conda activate nco_stable
 
 # Colours
-GREEN="\033[0;32m"
-RED="\033[0;31m"
-YELLOW="\033[1;33m"
+RED="\033[38;5;196m"     # ~ #e41a1c
+BLUE="\033[38;5;33m"     # ~ #377eb8
+YELLOW="\033[38;5;226m"  # ~ #ffff33
+GREEN="\033[38;5;34m"    # ~ #4daf4a
 RESET="\033[0m"
 
 input_base="/media/dafcluster4/storage/TraCE_22k_1500CE/chunk_out"
@@ -72,13 +73,13 @@ for chunk_dir in "${input_base}"/[0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][
             # concat outputs
             concat_tasmax="${tasmax_dir}/CHELSA_tasmax_${chunk_name}_concat.nc"
             concat_tasmin="${tasmin_dir}/CHELSA_tasmin_${chunk_name}_concat.nc"
-            echo -e "${GREEN}Concatenating tasmax in ${chunk_name} ...${RESET}"
+            echo -e "${GREEN}   Concatenating tasmax in ${chunk_name} ...${RESET}"
             cdo -s -w -b F32 -P 100 -O cat $(ls -v1 "${tasmax_dir}"/*.nc) "$concat_tasmax"
-            echo -e "${GREEN}Concatenating tasmin in ${chunk_name} ...${RESET}"
+            echo -e "${GREEN}   Concatenating tasmin in ${chunk_name} ...${RESET}"
             cdo -s -w -b F32 -P 100 -O cat $(ls -v1 "${tasmin_dir}"/*.nc) "$concat_tasmin"
         else
             concat_file="${var_dir}/CHELSA_${var}_${chunk_name}_concat.nc"
-            echo -e "${GREEN}Concatenating ${var} in ${chunk_name} ...${RESET}"
+            echo -e "   ${GREEN}Concatenating ${var} in ${chunk_name}...${RESET}"
             cdo -s -w -b F32 -P 100 -O cat $(ls -v1 "${var_dir}"/*.nc) "$concat_file"
         fi
     done
@@ -93,9 +94,9 @@ for chunk_dir in "${input_base}"/[0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][
                 echo "Input file for tasmax not found."
                 continue
             fi
-            echo "Bias correcting: $var"
-            echo "      Inputs: $(basename $input_file_tasmax), $(basename $input_file_tasmin)"
-            echo "      Deltas: $(basename $delta_file), $(basename $delta_file_dtr)"
+            echo -e "${GREEN}  Bias correcting: $var${RESET}"
+            echo -e "${BLUE}      Inputs: $(basename "$input_file_tasmax"), $(basename "$input_file_tasmin")${RESET}"
+            echo -e "${BLUE}      Deltas: $(basename "$delta_file"), $(basename "$delta_file_dtr")${RESET}"
             # temp files
             tmp_unpacked_tasmax=$(mktemp --suffix "_tasmax_unpacked.nc")
             tmp_unpacked_tasmin=$(mktemp --suffix "_tasmin_unpacked.nc")
@@ -119,7 +120,7 @@ for chunk_dir in "${input_base}"/[0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][
             cdo -P 100 -s -w -L "$remap_method","$grid_desc" "$delta_file_dtr" "$tmp_delta_dtr"
             # calculate tas = mean(tasmax,tasmin)
             cdo -O -b F32 -P 100 -s -w \
-                -settaxis,1500-01-16,,1month \
+                -settaxis,2000-01-16,,1month \
                 -setcalendar,365_day \
                 -setunit,'deg_C' \
                 -subc,273.15 \
@@ -171,9 +172,9 @@ for chunk_dir in "${input_base}"/[0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][
                 echo "Input file for ${var} not found."
                 continue
             fi
-            echo "Bias correcting: $var"
-            echo "      Input: $(basename $input_file)"
-            echo "      Delta: $(basename $delta_file)"
+            echo -e "${GREEN}  Bias correcting: $var${RESET}"
+            echo -e "${BLUE}        Input: $(basename $input_file)${RESET}"
+            echo -e "${BLUE}        Delta: $(basename $delta_file)${RESET}"
             # temp
             tmp_unpacked=$(mktemp --suffix "_${var}_unpacked.nc")
             tmp_delta=$(mktemp --suffix "_${var}_remap.nc")
@@ -209,20 +210,27 @@ done
 
 # concatentate the bias corrected files
 out_dir="/media/dafcluster4/storage/TraCE_22k_1500CE"
+unset variables
+variables=("pr" "tasmax" "tasmin")
 for var in "${variables[@]}"; do
-    echo "processing variable: ${var}..."
-
+    echo -e "${GREEN}Concatenating ${var}...${RESET}"
+    # create temp file and output
+    tmp_outvar=$(mktemp --suffix "_${var}_concat.nc")
     outfile="${out_dir}/out/${var}/TraCE_22ka_downscaled_${var}_decadal_21k_1500CE_biascorr.nc"
-    echo "outfile = ${outfile}"
-
+    echo -e "${BLUE}    Outfile: $(basename $outfile)${RESET}"
+    # store input order for debugging
     find "${input_base}"/*/out/"${var}" -type f -name '*biascorr.nc' | sort -V >"${out_dir}/${var}_concat_input_order.txt"
-
-    cdo -P 100 -L -s -O --absolute_taxis pack -cat -unpack \
+    # concat with CDO
+    cdo -P 100 -L -s -O \
+        -cat -unpack \
         $(find "${input_base}"/*/out/"${var}" -type f -name '*biascorr.nc' | sort -V) \
-        "${outfile}"
-
-    echo "resetting time dimension..."
-    ncap2 -O -s 'time=array(1,1,$time); time@units=""' "${outfile}" "${outfile}"
-
-    echo "Done for variable: ${var}"
+        "${tmp_outvar}"
+    # set time to 1...n with ncap2
+    echo -e "${GREEN}Resetting time dimension...${RESET}"
+    ncap2 -O -s 'time=array(1,1,$time); time@units=""' "${tmp_outvar}" "${tmp_outvar}"
+    # compress output
+    echo -e "${GREEN}Packing output file...${RESET}"
+    cdo -s -L -O -P 100 pack "${tmp_outvar}" "${outfile}"
+    rm -f "${tmp_outvar}"
+    echo -e "${YELLOW}Finished variable: ${var}${RESET}"
 done
