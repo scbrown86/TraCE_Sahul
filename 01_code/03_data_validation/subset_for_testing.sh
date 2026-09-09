8371e5e619a02875f6b7a32e60467e4a4167dd42
Stuart Brown <s.brown@adelaide.edu.au>
2026-09-09 15:52:36 +0930

    updates to test datasets

--- 01_code/03_data_validation/subset_for_testing.sh
+++ 01_code/03_data_validation/subset_for_testing.sh
@@ -10,41 +10,42 @@ GREEN="\033[38;5;34m"
 RESET="\033[0m"
 
 base_dir="/media/dafcluster4/storage/TraCE_22k_1500CE/out"
+base_dir="/mnt/Data/TraCE-Sahul"
 vars=(pr tasmax tasmin)
 
 for var in "${vars[@]}"; do
     echo -e "${GREEN}Processing $var...${RESET}"
-    files=$(ls "${base_dir}/${var}"/TraCE-Sahul_decadal_22k_1500CE_${var}*.nc | grep -v '_annSummary\.nc$' | sort -V)
+    files=$(ls "${base_dir}/${var}"/TraCE-Sahul_*_${var}*.nc | grep -v '_annSummary\.nc$' | sort -V)
     # files=$(ls "${base_dir}/${var}"/*.nc | grep -E "biascorr(_[0-9]+)?\.nc$")
     for f in $files; do
         echo -e "${YELLOW} Processing $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
         outdir="/home/dafcluster4/Desktop/TraCE-Sahul/$var"
         mkdir -p "$outdir"
         outfil="$outdir/$(basename "$f")"
-        cdo -L -s -w sellonlatbox,125.0,135.0,-13.0,-8.0 "$f" "$outfil"
+        cdo -L -s -w unpack -sellonlatbox,125.0,135.0,-13.0,-8.0 "$f" "$outfil"
         echo -e "${YELLOW} Finished $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
     done
     echo -e "${GREEN}Finished $var...${RESET}"
 done
 
-base_dir="/media/dafcluster4/storage/TraCE_1500_1990CE/1500_1990/out"
-for var in "${vars[@]}"; do
-    echo -e "${GREEN}Processing $var...${RESET}"
-    files=$(ls "${base_dir}/${var}"/TraCE-Sahul_1500_1990_${var}.nc | grep -v '_annSummary\.nc$' | sort -V)
-    # files=$(ls "${base_dir}/${var}"/*.nc | grep -E "biascorr.nc$")
-    for f in $files; do
-        echo -e "${YELLOW} Processing $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
-        outdir="/home/dafcluster4/Desktop/TraCE-Sahul/$var"
-        mkdir -p "$outdir"
-        outfil="$outdir/$(basename "$f")"
-        cdo -L -s -w settaxis,1500-01-01,,1mon \
-            -setcalendar,365_day \
-            -sellonlatbox,125.0,135.0,-13.0,-8.0 \
-            "$f" "$outfil"
-        echo -e "${YELLOW} Finished $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
-    done
-    echo -e "${GREEN}Finished $var...${RESET}"
-done
+# base_dir="/media/dafcluster4/storage/TraCE_1500_1990CE/1500_1990/out"
+# for var in "${vars[@]}"; do
+#     echo -e "${GREEN}Processing $var...${RESET}"
+#     files=$(ls "${base_dir}/${var}"/TraCE-Sahul_1500_1990_${var}.nc | grep -v '_annSummary\.nc$' | sort -V)
+#     # files=$(ls "${base_dir}/${var}"/*.nc | grep -E "biascorr.nc$")
+#     for f in $files; do
+#         echo -e "${YELLOW} Processing $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
+#         outdir="/home/dafcluster4/Desktop/TraCE-Sahul/$var"
+#         mkdir -p "$outdir"
+#         outfil="$outdir/$(basename "$f")"
+#         cdo -L -s -w settaxis,1500-01-01,,1mon \
+#             -setcalendar,365_day \
+#             -sellonlatbox,125.0,135.0,-13.0,-8.0 \
+#             "$f" "$outfil"
+#         echo -e "${YELLOW} Finished $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
+#     done
+#     echo -e "${GREEN}Finished $var...${RESET}"
+# done
 
 # Now pass the split files through the R script to correct the time-index which
 # would have been reset from the CDO subsetting
--- 01_code/03_data_validation/subset_fot_testing_cmip6.sh
+++ 01_code/03_data_validation/subset_fot_testing_cmip6.sh
@@ -9,7 +9,8 @@ YELLOW="\033[38;5;226m"
 GREEN="\033[38;5;34m"
 RESET="\033[0m"
 
-base_dir="/mnt/Data/CMIP6/bias_corrected/ensemble"
+# base_dir="/mnt/Data/CMIP6/bias_corrected/ensemble"
+base_dir="/mnt/Data/CMIP6/CMIP6-Sahul"
 vars=(pr tasmax tasmin)
 scenarios=(historical ssp126 ssp245 ssp370 ssp585)
 
@@ -17,11 +18,11 @@ for var in "${vars[@]}"; do
     echo -e "${GREEN}Processing $var...${RESET}"
     for scenario in "${scenarios[@]}"; do
         if [ "$scenario" = "historical" ]; then
-            daterange="190001-201412"
+            daterange="1990_2014"
         else
-            daterange="201501-210012"
+            daterange="2015_2100"
         fi
-        f="${base_dir}/${var}/${var}_ensmean_${scenario}_${daterange}.nc"
+        f="${base_dir}/${var}/TraCE-Sahul_${scenario}_${daterange}.nc"
         if [ ! -f "$f" ]; then
             echo -e "${RED} Missing $f, skipping...${RESET}"
             continue
@@ -30,7 +31,7 @@ for var in "${vars[@]}"; do
         outdir="/home/dafcluster4/Desktop/TraCE-Sahul/CMIP6/$var"
         mkdir -p "$outdir"
         outfil="$outdir/$(basename "$f")"
-        cdo -L -s sellonlatbox,125.0,135.0,-13.0,-8.0 "$f" "$outfil"
+        cdo -L -s unpack -sellonlatbox,125.0,135.0,-13.0,-8.0 "$f" "$outfil"
         echo -e "${YELLOW} Finished $(basename "$(dirname "$f")")/$(basename "$f")...${RESET}"
     done
     echo -e "${GREEN}Finished $var...${RESET}"
